6816ee737417f488565db1e123bc1dd7df07ed9f
bennyliya2060 <89083137+bennyliya2060@users.noreply.github.com>
2022-05-23 15:38:54 +0530

    fswebsite

--- Filesystem.php
+++ Filesystem.php
@@ -1,62 +1,275 @@
 <?php
 
+namespace Nextend\Framework\Filesystem;
 
-namespace Nextend\Framework\Cache\Storage;
+use Nextend\Framework\Filesystem\Joomla\JoomlaFilesystem;
+use Nextend\Framework\Filesystem\WordPress\WordPressFilesystem;
 
+class Filesystem {
 
-class Filesystem extends AbstractStorage {
+    /**
+     * @var AbstractPlatformFilesystem
+     */
+    private static $platformFilesystem;
 
     public function __construct() {
-        $this->paths['web']    = \Nextend\Framework\Filesystem\Filesystem::getWebCachePath();
-        $this->paths['notweb'] = \Nextend\Framework\Filesystem\Filesystem::getNotWebCachePath();
-        $this->paths['image']  = \Nextend\Framework\Filesystem\Filesystem::getImagesFolder();
+        self::$platformFilesystem = new WordPressFilesystem();
+
+        self::$platformFilesystem->init();
     }
 
-    public function isFilesystem() {
-        return true;
+    /**
+     * @return AbstractPlatformFilesystem
+     */
+    public static function get() {
+
+        return self::$platformFilesystem;
     }
 
-    public function clearAll($scope = 'notweb') {
-        if (\Nextend\Framework\Filesystem\Filesystem::existsFolder($this->paths[$scope])) {
-            \Nextend\Framework\Filesystem\Filesystem::deleteFolder($this->paths[$scope]);
-        }
+    public static function getPaths() {
+
+        return self::$platformFilesystem->getPaths();
     }
 
-    public function clear($group, $scope = 'notweb') {
+    public static function check($base, $folder) {
 
-        if (\Nextend\Framework\Filesystem\Filesystem::existsFolder($this->paths[$scope] . '/' . $group)) {
-            \Nextend\Framework\Filesystem\Filesystem::deleteFolder($this->paths[$scope] . '/' . $group);
-        }
+        self::$platformFilesystem->check($base, $folder);
     }
 
-    public function exists($group, $key, $scope = 'notweb') {
-        if (\Nextend\Framework\Filesystem\Filesystem::existsFile($this->paths[$scope] . '/' . $group . '/' . $key)) {
-            return true;
-        }
+    public static function measurePermission($testDir) {
 
-        return false;
+        self::$platformFilesystem->measurePermission($testDir);
     }
 
-    public function set($group, $key, $value, $scope = 'notweb') {
-        $path = $this->paths[$scope] . '/' . $group . '/' . $key;
-        $dir  = dirname($path);
-        if (!\Nextend\Framework\Filesystem\Filesystem::existsFolder($dir)) {
-            \Nextend\Framework\Filesystem\Filesystem::createFolder($dir);
-        }
-        \Nextend\Framework\Filesystem\Filesystem::createFile($path, $value);
+    /**
+     * @param $path
+     *
+     * @return string
+     */
+    public static function toLinux($path) {
+
+        return self::$platformFilesystem->toLinux($path);
     }
 
-    public function get($group, $key, $scope = 'notweb') {
-        return \Nextend\Framework\Filesystem\Filesystem::readFile($this->paths[$scope] . '/' . $group . '/' . $key);
+    /**
+     * @return string
+     */
+    public static function getBasePath() {
+
+        return self::$platformFilesystem->getBasePath();
     }
 
-    public function remove($group, $key, $scope = 'notweb') {
-        if ($this->exists($group, $key, $scope)) {
-            @unlink($this->paths[$scope] . '/' . $group . '/' . $key);
-        }
+    /**
+     * @param $path
+     */
+    public static function setBasePath($path) {
+
+        self::$platformFilesystem->setBasePath($path);
     }
 
-    public function getPath($group, $key, $scope = 'notweb') {
-        return $this->paths[$scope] . DIRECTORY_SEPARATOR . $group . DIRECTORY_SEPARATOR . $key;
+    public static function getWebCachePath() {
+
+        return self::$platformFilesystem->getWebCachePath();
+    }
+
+    public static function getNotWebCachePath() {
+
+        return self::$platformFilesystem->getNotWebCachePath();
+    }
+
+    /**
+     * @param $path
+     *
+     * @return string
+     */
+    public static function pathToAbsoluteURL($path) {
+
+        return self::$platformFilesystem->pathToAbsoluteURL($path);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return string
+     */
+    public static function pathToRelativePath($path) {
+
+        return self::$platformFilesystem->pathToRelativePath($path);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return string
+     */
+    public static function pathToAbsolutePath($path) {
+
+        return self::$platformFilesystem->pathToAbsolutePath($path);
+    }
+
+    /**
+     * @param $url
+     *
+     * @return string
+     */
+    public static function absoluteURLToPath($url) {
+
+        return self::$platformFilesystem->absoluteURLToPath($url);
+    }
+
+    /**
+     * @param $file
+     *
+     * @return bool
+     */
+    public static function fileexists($file) {
+
+        return self::$platformFilesystem->fileexists($file);
+    }
+
+    /**
+     * @param $file
+     *
+     * @return bool
+     */
+    public static function safefileexists($file) {
+
+        return self::$platformFilesystem->safefileexists($file);
+    }
+
+    /**
+     *
+     * @param $dir
+     *
+     * @return array Folder names without trailing slash
+     */
+    public static function folders($dir) {
+
+        return self::$platformFilesystem->folders($dir);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return bool
+     */
+    public static function is_writable($path) {
+
+        return self::$platformFilesystem->is_writable($path);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return bool
+     */
+    public static function createFolder($path) {
+
+        return self::$platformFilesystem->createFolder($path);
+    }
+
+    /**
+     * @param $dir
+     *
+     * @return bool
+     */
+    public static function deleteFolder($dir) {
+
+        return self::$platformFilesystem->deleteFolder($dir);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return bool
+     */
+    public static function existsFolder($path) {
+
+        return self::$platformFilesystem->existsFolder($path);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return array
+     */
+    public static function files($path) {
+
+        return self::$platformFilesystem->files($path);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return bool
+     */
+    public static function existsFile($path) {
+
+        return self::$platformFilesystem->existsFile($path);
+    }
+
+    /**
+     * @param $path
+     * @param $buffer
+     *
+     * @return int
+     */
+    public static function createFile($path, $buffer) {
+
+        return self::$platformFilesystem->createFile($path, $buffer);
+    }
+
+    /**
+     * @param $path
+     *
+     * @return string
+     */
+    public static function readFile($path) {
+
+        return self::$platformFilesystem->readFile($path);
+    }
+
+    /**
+     * convert dir alias to normal format
+     *
+     * @param $pathName
+     *
+     * @return mixed
+     */
+    public static function dirFormat($pathName) {
+
+        return self::$platformFilesystem->dirFormat($pathName);
+    }
+
+    public static function getImagesFolder() {
+
+        return self::$platformFilesystem->getImagesFolder();
+    }
+
+    public static function realpath($path) {
+
+        return self::$platformFilesystem->realpath($path);
+    }
+
+    public static function registerTranslate($from, $to) {
+
+        self::$platformFilesystem->registerTranslate($from, $to);
+    }
+
+    public static function convertToRealDirectorySeparator($path) {
+
+        return self::$platformFilesystem->convertToRealDirectorySeparator($path);
+    }
+
+    public static function get_temp_dir() {
+
+        return self::$platformFilesystem->get_temp_dir();
+    }
+
+    public static function tempnam($filename = '', $dir = '') {
+
+        return self::$platformFilesystem->tempnam($filename = '', $dir = '');
     }
 }
+
+new Filesystem();
